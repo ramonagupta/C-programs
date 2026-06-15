32d3235e97a2d17b4cf6851961187a35d7667066
ramonagupta <ramonagupta75@gmail.com>
2026-06-15 16:38:09 +0530

    Add files via upload
    To reverse order of elements inputted by user stored in one dimensional array

---
+++ 1D_Arrays/8.rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int a[10], n, i, temp;
+    int l, r;
+    printf("Enter size: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(l = 0, r = n - 1; l < r; l++, r--)
+    {
+        temp = a[l];
+        a[l] = a[r];
+        a[r] = temp;
+    }
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
