f75b50c977c55b2cb6f0935458df7fda4dec2cd1
ramonagupta <ramonagupta75@gmail.com>
2026-06-26 23:00:03 +0530

    Add files via upload
    To find average of elements inputted by user stored in two dimensional array

---
+++ 2D_Arrays/3.AvgOfElements.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10],i,j,n,m,sum=0;
+    float avg;
+    printf("enter no. of rows and columns");
+    scanf("%d%d",&n,&m);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            sum+=a[i][j];
+        }
+    }
+    avg = (float)sum/(n*m);
+    printf("%f",avg);
+}
