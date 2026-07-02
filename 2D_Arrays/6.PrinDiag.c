f1257134d778af7480ef070d2d1d4703888d945f
ramonagupta <ramonagupta75@gmail.com>
2026-07-02 23:25:40 +0530

    Add files via upload
    To print principal diagonal of matrix of numbers inputted by user stored in two dimensional array

---
+++ 2D_Arrays/6.PrinDiag.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10],i,j,n;
+    printf("enter no.s of rows");
+    scanf("%d",&n);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i][i]);//running double i
+    }
+}
