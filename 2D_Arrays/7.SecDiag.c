1eac8427137346a1a98295c4b3ea7ebd39251bea
ramonagupta <ramonagupta75@gmail.com>
2026-07-03 23:28:52 +0530

    Add files via upload
    To print elements of secondary diagonal of matrix of number inputted by user stored as two dimensional array

---
+++ 2D_Arrays/7.SecDiag.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10],i,j,n;
+    printf("enter no.s of rows");
+    scanf("%d",&n);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i+j==n-1)
+            printf("%d",a[i][j]);//i+j=n-1
+        }
+    }
+    //optimised way
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i][n-i-1]);
+    }
+}
