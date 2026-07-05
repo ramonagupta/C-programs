629eb89220959e7cf3ab2a9b84a1cbc62ad79aa9
ramonagupta <ramonagupta75@gmail.com>
2026-07-05 22:54:58 +0530

    Print lower triangular matrix
    To print elements of a lower triangular matrix with respect to principal diagonal of matrix made of numbers inputted by user stored in two dimensional array

---
+++ 2D_Arrays/9.LowerTriPD.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10],i,j,n;
+    printf("enter no.s of rows");
+    scanf("%d",&n);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i>j)
+            printf("%d",a[i][j]);
+        }
+    }
+    printf("\n");
+    //diagonal included
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i>=j)
+            printf("%d",a[i][j]);
+        }
+    }
+    printf("\n");
+    //optimised 
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            printf("%d",a[j][i]);//switch i and j from last code
+        }
+    }
+}
