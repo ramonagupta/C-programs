d37166a8f4b43eec781594d9df091aef07f52c05
ramonagupta <ramonagupta75@gmail.com>
2026-07-22 22:23:47 +0530

    To check if a matrix is dense
    To check if matrix inputted by user is dense or not

---
+++ 2D_Arrays/22.DenseMat.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10],i,j,n,m,cz=0,cn=0;
+    printf("enter no.s of rows and columns");
+    scanf("%d%d",&n,&m);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            if(a[i][j]==0)
+            cz++;
+            else
+            cn++;
+        }
+    }
+    if(cz<cn)
+    printf("dense matrix");
+    else
+    printf("not dense matrix");
+}
