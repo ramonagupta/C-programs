2f762c1dc81d7c8c7b755152435ea203678dd4d3
ramonagupta <ramonagupta75@gmail.com>
2026-08-19 21:53:38 +0530

    Realloc Function
    To use realloc function to resize array made using DMA

---
+++ Dynamic Memory Allocation/4.Realloc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,*p,i;
+    printf("enter number of integers");
+    scanf("%d",&n);
+    p=(int*)malloc(n*sizeof(int));
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",p+i);
+    }
+    printf("enter new number of elements");
+    scanf("%d",&n);
+    realloc(p,n);
+    printf("enter values");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",p+i);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",*(p+i));
+    }
+    free(p);
+}
